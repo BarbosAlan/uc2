c730a5c7c493c2f4f9230c51df127fb78bd5ff68
Alan Barbosa <149962269+BarbosAlan@users.noreply.github.com>
2025-02-26 16:14:47 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 /*window.onload = function() {
     alert("Seja bem-vindo à Jornada Viagens! ✈✈");
 }*/
+
+function exibirDados(){
+
+    let nome = document.getElementById("nome").value;
+    let tel = document.getElementById("movel").value;
+    let email = document.getElementById("correio").value;
+
+    document.write("<h2>Dados Recebidos:</h2>");
+    document.write("<p><strong>Nome:</strong>" + nome + "</p>");
+    document.write("<p><strong>Telefone:</strong>" +tel + "</p>");
+    document.write("<p><strong>E-mail:</strong>" + email + "</p>");
+}
